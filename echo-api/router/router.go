0c054581d9642f6febe2d7ac4f92ca93d4077024
ito0804takuya <ito0804takuya@gmail.com>
2023-11-26 08:59:11 +0900

    Routerを実装

---
+++ echo-api/router/router.go
@@ -0,0 +1,17 @@
+package router
+
+import (
+	"echo-api/controller"
+
+	"github.com/labstack/echo/v4"
+)
+
+func NewRouter(uc controller.IUserController) *echo.Echo {
+	e := echo.New()
+
+	e.POST("/signup", uc.SignUp)
+	e.POST("/login", uc.LogIn)
+	e.POST("/logout", uc.LogOut)
+
+	rerutn e
+}
