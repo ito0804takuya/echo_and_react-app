3908072809fac4efd83a63b7132d508c2db73ae6
ito0804takuya <ito0804takuya@gmail.com>
2023-11-25 18:43:20 +0900

    userUsecaseを作成

--- echo-api/go.mod
+++ echo-api/go.mod
@@ -3,6 +3,8 @@ module echo-api
 go 1.20
 
 require (
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	golang.org/x/crypto v0.14.0
 	gorm.io/driver/postgres v1.5.4
 	gorm.io/gorm v1.25.5
 )
@@ -11,7 +13,6 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 )
 
---
+++ echo-api/usecase/user_usecase.go
@@ -0,0 +1,72 @@
+package usecase
+
+import (
+	"echo-api/model"
+	"echo-api/repository"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type IUserUsecase interface {
+	SignUp(user model.User) (model.UserResponse, error)
+	Login(user model.User) (string, error)
+}
+
+// Usecaseが（Repositoryでなく）Repositoryのインターフェースに依存する
+type userUsecase struct {
+	ur repository.IUserRepository
+}
+
+// Repositoryのインターフェースを注入（DI）するためのコンストラクタ
+func NewUserUsecase(ur repository.IUserRepository) IUserUsecase {
+	return &userUsecase{ur}
+}
+
+func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
+	// 平文のパスワードをハッシュ化
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		return model.UserResponse{}, err
+	}
+
+	newUser := model.User{Email: user.Email, Password: string(hash)}
+	if err := uu.ur.CreateUser(&newUser); err != nil {
+		return model.UserResponse{}, err
+	}
+
+	// レスポンス用に加工
+	resUser := model.UserResponse{
+		ID:    newUser.ID,
+		Email: newUser.Email,
+	}
+	return resUser, nil
+}
+
+func (uu *userUsecase) Login(user model.User) (string, error) {
+	storedUser := model.User{}
+	if err := uu.ur.GetUserByEmail(&storedUser, user.Email); err != nil {
+		return "", err
+	}
+
+	// 入力されたパスワードと保存されている（ユーザの）パスワードが等しいかチェック
+	err := bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(user.Password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		// トークンのペイロード
+		"user_id": storedUser.ID,
+		"exp":     time.Now().Add(time.Hour * 12).Unix(), // トークン有効期限
+	})
+	// tokenStringはユーザに返却する認証情報トークン
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
