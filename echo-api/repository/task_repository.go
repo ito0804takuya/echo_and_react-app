8b7c3334cac64cdcba91ebaef0ae3b6fd377dea3
ito0804takuya <ito0804takuya@gmail.com>
2023-11-28 07:19:35 +0900

    taskRepository実装

---
+++ echo-api/repository/task_repository.go
@@ -0,0 +1,71 @@
+package repository
+
+import (
+	"echo-api/model"
+	"fmt"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type ITaskRepository interface {
+	GetAllTasks(tasks *[]model.Task, userId uint) error
+	GetTaskById(task *model.Task, userId uint, taskId uint) error
+	CreateTask(task *model.Task) error
+	UpdateTask(task *model.Task, userId uint, taskId uint) error
+	DeleteTask(userId uint, taskId uint) error
+}
+
+type taskRepository struct {
+	db *gorm.DB
+}
+
+func NewTaskRepository(db *gorm.DB) ITaskRepository {
+	return &taskRepository{db}
+}
+
+func (tr *taskRepository) GetAllTasks(tasks *[]model.Task, userId uint) error {
+	if err := tr.db.Joins("User").Where("user_id=?", userId).Order("created_at").Find(tasks).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tr *taskRepository) GetTaskById(task *model.Task, userId uint, taskId uint) error {
+	if err := tr.db.Joins("User").Where("user_id=?", userId).First(task, taskId).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tr *taskRepository) CreateTask(task *model.Task) error {
+	if err := tr.db.Create(task).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tr *taskRepository) UpdateTask(task *model.Task, userId uint, taskId uint) error {
+	// .Model() : 更新するモデル（テーブル）を指定
+	// .Clauses(clause.Returning{}) : updateを実行したレコードを取得
+	result := tr.db.Model(task).Clauses(clause.Returning{}).Where("id=? AND user_id=?", taskId, userId).Update("title", task.Title)
+	if result.Error != nil {
+		return result.Error
+	}
+	// 更新対象のレコードが存在しないことについては、Errorにならない仕様なので、RowsAffected（実際に更新を行ったレコード数）が1より小さい場合はエラーを返す
+	if result.RowsAffected < 1 {
+		return fmt.Errorf("object does not exist")
+	}
+	return nil
+}
+
+func (tr *taskRepository) DeleteTask(userId uint, taskId uint) error {
+	result := tr.db.Where("id=? AND user_id=?", taskId, userId).Delete(&model.Task{})
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected < 1 {
+		return fmt.Errorf("object does not exist")
+	}
+	return nil
+}
