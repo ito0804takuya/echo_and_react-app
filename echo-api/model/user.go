1c8b1b0ef5ee5b09c04cef1a617b4e37a9bd2c1a
ito0804takuya <ito0804takuya@gmail.com>
2023-11-27 19:18:05 +0900

    タイポ修正

--- echo-api/model/user.go
+++ echo-api/model/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	// json:"id" : jsonに変換するとidという小文字のキーにするように指定
 	// gorm:"primaryKey : gormを使うときに主キーとして認識される
-	ID        uint      `json:"id" gorm:"primaryKey`
+	ID        uint      `json:"id" gorm:"primaryKey"`
 	Email     string    `json:"email" gorm:"unique"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
@@ -14,6 +14,6 @@ type User struct {
 
 // レスポンス用のユーザ型
 type UserResponse struct {
-	ID    uint   `json:"id" gorm:"primaryKey`
+	ID    uint   `json:"id" gorm:"primaryKey"`
 	Email string `json:"email" gorm:"unique"`
 }
